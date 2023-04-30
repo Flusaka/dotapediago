1f8266e1468cc8ee31c78aec2c4e7334527a6c73
Neil Armitage <flusaka@gmail.com>
2023-04-30 14:05:46 +0300

    Add support for retrieving matches up until a certain time

--- matches.go
+++ matches.go
@@ -30,6 +30,11 @@ type Match struct {
 }
 
 func (client *Client) GetMatches() ([]*Match, error) {
+	// Add a year for the default GetMatches call
+	return client.GetMatchesUntil(time.Now().Add(time.Hour * 24 * 365))
+}
+
+func (client *Client) GetMatchesUntil(maxStartTime time.Time) ([]*Match, error) {
 	doc, err := client.getDocument(matchesEndpoint)
 
 	if err != nil {
@@ -39,20 +44,21 @@ func (client *Client) GetMatches() ([]*Match, error) {
 	// This is a little awkward, and works with how the current HTML structure is, but might need reworking, need to investigate data queries
 	matchesGroups := doc.Find(".matches-list").Children().Last().Children()
 	// First one is _all_ matches of any "prestige"
-	matches := client.parseMatches(matchesGroups.Eq(0))
+	matches := client.parseMatches(matchesGroups.Eq(0), maxStartTime)
 	return matches, nil
 }
 
-func (client *Client) parseMatches(table *goquery.Selection) []*Match {
+func (client *Client) parseMatches(table *goquery.Selection, maxStartTime time.Time) []*Match {
 	var matches []*Match
 	table.Find(".infobox_matches_content").Each(func(i int, selection *goquery.Selection) {
-		match := client.parseMatch(selection)
-		matches = append(matches, match)
+		if match, shouldAdd := client.parseMatch(selection, maxStartTime); shouldAdd {
+			matches = append(matches, match)
+		}
 	})
 	return matches
 }
 
-func (client *Client) parseMatch(row *goquery.Selection) *Match {
+func (client *Client) parseMatch(row *goquery.Selection, maxStartTime time.Time) (*Match, bool) {
 	// Parse the teams first
 	status := MatchStatusUpcoming
 
@@ -61,20 +67,26 @@ func (client *Client) parseMatch(row *goquery.Selection) *Match {
 
 	timerObject := row.Find(".timer-object")
 
-	var streamUrl = ""
-	twitchStream, exists := timerObject.Attr("data-stream-twitch")
-	if exists {
-		streamUrl, _ = client.GetStreamURL(fmt.Sprintf(streamEndpointFmt, twitchStream))
-	}
-
 	var startTime time.Time
 	startTimestampStr, exists := timerObject.Attr("data-timestamp")
 	if exists {
 		startTimestamp, _ := strconv.ParseInt(startTimestampStr, 10, 64)
 		startTime = time.Unix(startTimestamp, 0)
+		if startTime.After(maxStartTime) {
+			return nil, false
+		}
 		if startTime.Before(time.Now()) {
 			status = MatchStatusOngoing
 		}
+	} else {
+		// If the timestamp doesn't exist for some reason, return false for whether it should be added to the matches array
+		return nil, false
+	}
+
+	var streamUrl = ""
+	twitchStream, exists := timerObject.Attr("data-stream-twitch")
+	if exists {
+		streamUrl, _ = client.GetStreamURL(fmt.Sprintf(streamEndpointFmt, twitchStream))
 	}
 
 	var bestOf = 0
@@ -95,7 +107,7 @@ func (client *Client) parseMatch(row *goquery.Selection) *Match {
 		Status:         status,
 		BestOf:         bestOf,
 		TournamentName: tournamentName,
-	}
+	}, true
 }
 
 func parseTeam(teamSelector *goquery.Selection) *BaseTeam {
